2dc233a073d3334102e6f714cb78d55a0763b8f0
qs <mosdit@gmail.com>
2014-08-22 21:24:12 +0400

    init

--- csps.py
+++ csps.py
@@ -1,12 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def hello():
+    return 'Just hello!'
 
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
