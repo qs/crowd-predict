e9e6db8e8f2f6c4bb739e0ae14b90dd7b113d42c
thaelathy <hnalina@gmail.com>
2014-08-23 22:39:16 +0400

    form

--- crowd-predict/forms.py
+++ crowd-predict/forms.py
@@ -2,7 +2,13 @@ from flask_wtf import Form
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
+
 class EventForm(Form):
     event_key = StringField('event_key', validators=[DataRequired()])
     title = StringField('title', validators=[DataRequired()])
     available_answers = TextAreaField('available_answers', validators=[DataRequired()])
+
+
+class LoginForm(Form):
+    username = StringField('username', validators=[DataRequired()])
+    passwd = StringField('passwd', validators=[DataRequired()])
